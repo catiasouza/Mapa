e932212021dd11928d65722d4d493a72ba0ff452
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-10-17 10:50:13 -0300

    Capturando localizacao real

--- Mapas/ViewController.swift
+++ Mapas/ViewController.swift
@@ -16,21 +16,12 @@ class ViewController: UIViewController , MKMapViewDelegate, CLLocationManagerDel
         gerenciasdorLocal.desiredAccuracy = kCLLocationAccuracyBest //PRECISAO DA LOCALIZACAO
         gerenciasdorLocal.requestWhenInUseAuthorization()       //PEDINDO AUTORIZACAO AO USUARIO
         gerenciasdorLocal.startUpdatingLocation()
+        
+        
         //CONFIGURACAO ESTATICA
         
-//        let latitude: CLLocationDegrees = -23.539995
-//        let longitude: CLLocationDegrees = -46.597079
 //
-//        let deltaLatitude: CLLocationDegrees = 0.05
-//        let deltaLongitude: CLLocationDegrees = 0.05
-//
-//        let localizacao: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
-//        let areaVisualizacao: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: deltaLatitude , longitudeDelta: deltaLongitude)
-//
-//        let regiao:MKCoordinateRegion = MKCoordinateRegion(center: localizacao, span: areaVisualizacao)
-       
-        //mapa.setRegion(regiao, animated: true)
-       // let latitude: CLLocationDegrees = -23.539995
+        // let latitude: CLLocationDegrees = -23.539995
        // let longitude: CLLocationDegrees = -46.597079
         
         //let deltaLatitude: CLLocationDegrees = 0.008
@@ -55,5 +46,24 @@ class ViewController: UIViewController , MKMapViewDelegate, CLLocationManagerDel
        
         
 }
+    // metodo para pegar localizacao real
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        let localizacaoUsuario: CLLocation = locations.last! //ultima localizacao usuario
+        
+        // MONTA A EXIBICAO DO MAPA
+        let latitude: CLLocationDegrees = localizacaoUsuario.coordinate.latitude
+        let longitude: CLLocationDegrees = localizacaoUsuario.coordinate.longitude
+        
+        let deltaLatitude: CLLocationDegrees = 0.05
+        let deltaLongitude: CLLocationDegrees = 0.05
+        
+        let localizacao: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+        let areaVisualizacao: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: deltaLatitude , longitudeDelta: deltaLongitude)
+        
+        let regiao:MKCoordinateRegion = MKCoordinateRegion(center: localizacao, span: areaVisualizacao)
+        
+        mapa.setRegion(regiao, animated: true)
+    }
 
 }
