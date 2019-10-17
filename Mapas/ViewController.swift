d5c1d57e0f31c2f84efcf1402a029295abaf10b6
Catia Miranda de Souza <catiamiranda@NBBV030098.local>
2019-10-17 09:44:15 -0300

    Finalizando a localizacao estatica

--- Mapas/ViewController.swift
+++ Mapas/ViewController.swift
@@ -2,7 +2,7 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , MKMapViewDelegate{
     
     
     @IBOutlet weak var mapa: MKMapView!
@@ -10,21 +10,39 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+//        let latitude: CLLocationDegrees = -23.539995
+//        let longitude: CLLocationDegrees = -46.597079
+//
+//        let deltaLatitude: CLLocationDegrees = 0.05
+//        let deltaLongitude: CLLocationDegrees = 0.05
+//
+//        let localizacao: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
+//        let areaVisualizacao: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: deltaLatitude , longitudeDelta: deltaLongitude)
+//
+//        let regiao:MKCoordinateRegion = MKCoordinateRegion(center: localizacao, span: areaVisualizacao)
+       
+        //mapa.setRegion(regiao, animated: true)
         let latitude: CLLocationDegrees = -23.539995
         let longitude: CLLocationDegrees = -46.597079
         
-        let deltaLatitude: CLLocationDegrees = 0.05
-        let deltaLongitude: CLLocationDegrees = 0.05
+        let deltaLatitude: CLLocationDegrees = 0.008
+        let deltaLongitude: CLLocationDegrees = 0.008
         
         let localizacao: CLLocationCoordinate2D = CLLocationCoordinate2DMake(latitude, longitude)
-        let areaVisualizacao: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: deltaLatitude , longitudeDelta: deltaLongitude)
+        let areaVisualizacao: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: deltaLatitude, longitudeDelta: deltaLongitude)
         
-        let regiao:MKCoordinateRegion = MKCoordinateRegion(center: localizacao, span: areaVisualizacao)
-       
+        let regiao: MKCoordinateRegion = MKCoordinateRegion(center: localizacao, span: areaVisualizacao)
+
         mapa.setRegion(regiao, animated: true)
+        
+        //CONFIGURACAO ANOTACAO
+        let anotacao = MKPointAnnotation()
+        anotacao.coordinate = localizacao
+        anotacao.title = "Home"
+        anotacao.subtitle = "sweet Home"
+        mapa.addAnnotation( anotacao)
        
-    }
-
-
+        
 }
 
+}
